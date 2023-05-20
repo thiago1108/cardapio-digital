225e2849285a149216826c6328288a2787588f8e
Thiago Prado de Mello Branco <thiagopradodemellobranco@MacBook-Pro-de-Thiago.local>
2023-05-20 15:01:01 -0300

    Projeto inicial

---
+++ src/main/java/com/example/cardapio/CardapioApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // essa é nossa classe de inicio de tudo
+public class CardapioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioApplication.class, args);
+	}
+
+}
